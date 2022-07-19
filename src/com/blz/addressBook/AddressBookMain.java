0f2b4ec661bde823162b38360a28b2b76d865e80
akashsathe063 <akashsathe063@gmail.com>
2022-07-19 22:05:19 +0530

    Contact Address

--- src/com/blz/addressBook/AddressBookMain.java
+++ src/com/blz/addressBook/AddressBookMain.java
@@ -1,8 +1,41 @@
 package com.blz.addressBook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
+    public static String firstName;
+    public static String lastName;
+    public static String city;
+    public static String state;
+    public static String address;
+    public static int zip;
+    public static long phoneNumber;
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Address book Program");
+        System.out.println("Welcome to address book program ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a first Name");
+        firstName = sc.next();
+        System.out.println("first name: " + firstName);
+        System.out.println("Enter a Last name");
+        lastName = sc.next();
+        System.out.println("last namre: " + lastName);
+        System.out.println("enter a city");
+        city = sc.next();
+        System.out.println("city: " + city);
+        System.out.println("enter a state");
+        state = sc.next();
+        System.out.println("state: " + state);
+        sc.nextLine();
+        System.out.println("Enter a Address");
+        address = sc.nextLine();
+        System.out.println("address: " + address);
+        System.out.println("enter a zip code");
+        zip = sc.nextInt();
+        System.out.println("zip: " + zip);
+        System.out.println("Enter a Phone number");
+        phoneNumber = sc.nextLong();
+        System.out.println("phone number: " + phoneNumber);
+
     }
 }
-
