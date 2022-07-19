584a4c1e14872dcabed43944e06841b92bf8af3d
akashsathe063 <akashsathe063@gmail.com>
2022-07-19 21:54:57 +0530

    welcome to address book program

---
+++ src/com/blz/addressBook/AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.blz.addressBook;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address book Program");
+    }
+}
+
