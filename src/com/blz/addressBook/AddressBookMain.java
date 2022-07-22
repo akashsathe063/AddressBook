aa28eb50999e0ed7ab56dbc44485472acbfa5f06
akashsathe063 <akashsathe063@gmail.com>
2022-07-22 19:15:40 +0530

    Add new Contacts to address book

--- src/com/blz/addressBook/AddressBookMain.java
+++ src/com/blz/addressBook/AddressBookMain.java
@@ -1,41 +1,89 @@
 package com.blz.addressBook;
 
+import java.util.HashSet;
+import java.util.Objects;
 import java.util.Scanner;
+import java.util.Set;
 
 public class AddressBookMain {
-    public static String firstName;
-    public static String lastName;
-    public static String city;
-    public static String state;
-    public static String address;
-    public static int zip;
-    public static long phoneNumber;
-
+   static Set<PersonContact> addressBook = new HashSet<>();
     public static void main(String[] args) {
+        AddressBookMain addressBookMain = new AddressBookMain();
         System.out.println("Welcome to address book program ");
+        addressBook.add(addressBookMain.consolInput());
+        addressBook.add(addressBookMain.consolInput());
+        addressBook.add(addressBookMain.consolInput());
+
+        addressBookMain.displaypPersionDetails();
+    }
+    public  PersonContact consolInput() {
         Scanner sc = new Scanner(System.in);
+        PersonContact personDetail = new PersonContact();
         System.out.println("Enter a first Name");
-        firstName = sc.next();
-        System.out.println("first name: " + firstName);
+       personDetail.firstName = sc.next();
+        System.out.println("first name: " +personDetail.firstName);
         System.out.println("Enter a Last name");
-        lastName = sc.next();
-        System.out.println("last namre: " + lastName);
+        personDetail.lastName = sc.next();
+        System.out.println("last namre: " +personDetail.lastName);
         System.out.println("enter a city");
-        city = sc.next();
-        System.out.println("city: " + city);
+        personDetail.city = sc.next();
+        System.out.println("city: " +personDetail.city);
         System.out.println("enter a state");
-        state = sc.next();
-        System.out.println("state: " + state);
+        personDetail.state = sc.next();
+        System.out.println("state: " +personDetail.state);
         sc.nextLine();
         System.out.println("Enter a Address");
-        address = sc.nextLine();
-        System.out.println("address: " + address);
+        personDetail.address = sc.nextLine();
+        System.out.println("address: " +personDetail.address);
         System.out.println("enter a zip code");
-        zip = sc.nextInt();
-        System.out.println("zip: " + zip);
+        personDetail.zip = sc.nextInt();
+        System.out.println("zip: " +personDetail.zip);
         System.out.println("Enter a Phone number");
-        phoneNumber = sc.nextLong();
-        System.out.println("phone number: " + phoneNumber);
+        personDetail.phoneNumber = sc.nextLong();
+        System.out.println("phone number: " +personDetail.phoneNumber);
+      return personDetail;
+    }
 
+     public void displaypPersionDetails(){
+         for (PersonContact persioDetail : addressBook) {
+             System.out.println(persioDetail);
+         }
+     }
+}
+
+class PersonContact {
+    public String firstName;
+    public String lastName;
+    public    String city;
+    public    String state;
+    public    String address;
+    public    int zip;
+    public    long phoneNumber;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PersonContact)) return false;
+        PersonContact contact = (PersonContact) o;
+        return phoneNumber == contact.phoneNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(phoneNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "PersonContact{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", address='" + address + '\'' +
+                ", zip=" + zip +
+                ", phoneNumber=" + phoneNumber +
+                '}';
     }
 }
+
