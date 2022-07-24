54bfefa6fd532dbf720dc2c92de00cd02730fcfc
akashsathe063 <akashsathe063@gmail.com>
2022-07-24 16:05:32 +0530

    Edit contact

--- src/com/blz/addressBook/AddressBookMain.java
+++ src/com/blz/addressBook/AddressBookMain.java
@@ -1,77 +1,103 @@
 package com.blz.addressBook;
 
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class AddressBookMain {
-   static Set<PersonContact> addressBook = new HashSet<>();
+    List<PersonContact> addressBook = new ArrayList<>();
+    String persionName;
+
     public static void main(String[] args) {
         AddressBookMain addressBookMain = new AddressBookMain();
         System.out.println("Welcome to address book program ");
-        addressBook.add(addressBookMain.consolInput());
-        addressBook.add(addressBookMain.consolInput());
-        addressBook.add(addressBookMain.consolInput());
+        int option;
+        do {
+            System.out.println("1.AddContac\n2.editContact\n3.display\n4.exit");
+            Scanner scanner = new Scanner(System.in);
+            option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("Add a contact");
+                    addressBookMain.addContact();
+                    break;
+                case 2:
+                    System.out.println("edit contact");
+                    addressBookMain.editPersionContact(addressBookMain.addressBook);
+                    break;
+                case 3:
+                    addressBookMain.displaypPersionDetails();
+                    break;
+                default:
+                    System.out.println("choose from menue");
+                    break;
+            }
 
-        addressBookMain.displaypPersionDetails();
+        } while (option != 4);
     }
-    public  PersonContact consolInput() {
+
+    public PersonContact consolInput() {
         Scanner sc = new Scanner(System.in);
         PersonContact personDetail = new PersonContact();
         System.out.println("Enter a first Name");
-       personDetail.firstName = sc.next();
-        System.out.println("first name: " +personDetail.firstName);
+        personDetail.firstName = sc.next();
+        System.out.println("first name: " + personDetail.firstName);
         System.out.println("Enter a Last name");
         personDetail.lastName = sc.next();
-        System.out.println("last namre: " +personDetail.lastName);
+        System.out.println("last namre: " + personDetail.lastName);
         System.out.println("enter a city");
         personDetail.city = sc.next();
-        System.out.println("city: " +personDetail.city);
+        System.out.println("city: " + personDetail.city);
         System.out.println("enter a state");
         personDetail.state = sc.next();
-        System.out.println("state: " +personDetail.state);
+        System.out.println("state: " + personDetail.state);
         sc.nextLine();
         System.out.println("Enter a Address");
         personDetail.address = sc.nextLine();
-        System.out.println("address: " +personDetail.address);
+        System.out.println("address: " + personDetail.address);
         System.out.println("enter a zip code");
         personDetail.zip = sc.nextInt();
-        System.out.println("zip: " +personDetail.zip);
+        System.out.println("zip: " + personDetail.zip);
         System.out.println("Enter a Phone number");
         personDetail.phoneNumber = sc.nextLong();
-        System.out.println("phone number: " +personDetail.phoneNumber);
-      return personDetail;
+        System.out.println("phone number: " + personDetail.phoneNumber);
+        return personDetail;
     }
 
-     public void displaypPersionDetails(){
-         for (PersonContact persioDetail : addressBook) {
-             System.out.println(persioDetail);
-         }
-     }
+    public void addContact() {
+        addressBook.add(consolInput());
+    }
+
+    public void displaypPersionDetails() {
+        for (PersonContact persioDetail : addressBook) {
+            System.out.println(persioDetail);
+        }
+    }
+
+    public void editPersionContact(List<PersonContact> list) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a name");
+        String persionName = scanner.next();
+        getPersionContact(persionName, list);
+    }
+
+    public void getPersionContact(String persionName, List<PersonContact> list) {
+        for (int i = 0; i < list.size(); i++) {
+            if (persionName.equals(list.get(i).firstName)) {
+                list.remove(i);
+                list.add(i, consolInput());
+            } else
+                System.out.println("contact not available");
+        }
+    }
 }
 
 class PersonContact {
     public String firstName;
     public String lastName;
-    public    String city;
-    public    String state;
-    public    String address;
-    public    int zip;
-    public    long phoneNumber;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof PersonContact)) return false;
-        PersonContact contact = (PersonContact) o;
-        return phoneNumber == contact.phoneNumber;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(phoneNumber);
-    }
+    public String city;
+    public String state;
+    public String address;
+    public int zip;
+    public long phoneNumber;
 
     @Override
     public String toString() {
